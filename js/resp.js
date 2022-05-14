e3719f76f176aea34549a8ad75ec2df400112c5a
MrBadal <badalpal42@yahoo.in>
2022-05-15 00:33:24 +0530

    portfolio-demo

---
+++ js/resp.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-bar").style.top = "0";
+  } else {
+    document.getElementById("nav-bar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
